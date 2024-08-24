ba9274b34ae5cbc59ba0e2759f27b91d39c0877f
Nicholas Martens <nicholas.martens2@gmail.com>
2024-08-24 21:33:32 +0200

    Implement Discord webhook notifications

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
-const https = require("https");
+const axios = require("axios");
 const fs = require("fs");
 require("dotenv").config();
 
-const URL = process.env.API_URL;
-const INTERVAL = process.env.API_URL || 60000;
+const API_URL = process.env.API_URL;
+const INTERVAL = process.env.INTERVAL || 60000;
+const TAIL_LENGTH = parseInt(process.env.TAIL_LENGTH, 10) || 10;
+const THRESHOLD = process.env.THRESHOLD || 10000; // When average response time is 10s, send notification.
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+const NOTIFICATION_COOLDOWN = process.env.NOTIFICATION_COOLDOWN || 360000;
+
+let latestResponseTimes = [];
+let latestNotificationTime = new Date(0);
 
 function logResponseTime() {
   const startTime = Date.now();
 
-  https
-    .get(URL, (res) => {
+  axios
+    .get(API_URL)
+    .then((res) => {
       const endTime = Date.now();
       const responseTime = endTime - startTime;
+
+      addResponseTime(responseTime);
 
       const date = new Date();
       const dateString = date.toISOString().substring(0, 10);
@@ -24,12 +34,58 @@ function logResponseTime() {
 
       console.log(logMessage.trim());
     })
-    .on("error", (err) => {
+    .catch((err) => {
       console.error("Error:", err.message);
     });
 }
 
-if (URL) {
+function addResponseTime(responseTime) {
+  latestResponseTimes.push(responseTime);
+
+  if (latestResponseTimes.length > TAIL_LENGTH) {
+    latestResponseTimes.shift();
+  }
+
+  const average =
+    latestResponseTimes.reduce((acc, time) => acc + time, 0) /
+    latestResponseTimes.length;
+
+  if (latestResponseTimes.length === TAIL_LENGTH) {
+    if (average > THRESHOLD) {
+      const currentTime = new Date();
+      const cooldownPeriod = NOTIFICATION_COOLDOWN;
+      if (currentTime - latestNotificationTime >= cooldownPeriod) {
+        sendNotification(average);
+        latestNotificationTime = currentTime;
+      }
+    }
+  }
+}
+
+function sendNotification(average) {
+  if (!DISCORD_WEBHOOK_URL) {
+    return;
+  }
+
+  const message = `Average response time is ${average}ms`;
+
+  const data = JSON.stringify({ content: message });
+
+  axios
+    .post(DISCORD_WEBHOOK_URL, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => {
+      console.log(`Notification sent, status: ${res.status}`);
+    })
+    .catch((err) => {
+      console.error("Error sending notification:", err.message);
+    });
+}
+
+if (API_URL) {
   setInterval(logResponseTime, INTERVAL);
 } else {
   console.error(
